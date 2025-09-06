465176103a2f6aa7a468b88e942e26ee2c652271
ikhsan <ikhsanmuhamad3885@gmail.com>
2025-09-06 12:34:41 +0700

    add: sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import Sidebar from "./components/Sidebar";
+import { Box, CssVarsProvider } from "@mui/joy";
+import { CssBaseline } from "@mui/material";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <CssVarsProvider disableTransitionOnChange>
+        <CssBaseline />
+        <Box sx={{ display: "flex", minHeight: "100dvh" }}>
+          <Sidebar />
+          <Header />
+          <Box
+            component="main"
+            className="MainContent"
+            sx={{
+              pt: { xs: "calc(12px + var(--Header-height))", md: 3 },
+              pb: { xs: 2, sm: 2, md: 3 },
+              flex: 1,
+              display: "flex",
+              flexDirection: "column",
+              minWidth: 0,
+              height: "100dvh",
+              gap: 1,
+              overflow: "auto",
+              bgcolor: "background.body",
+              color: "text.primary",
+            }}
+          >
+            <MainPage />
+          </Box>
+        </Box>
+      </CssVarsProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,54 @@
+import Typography from "@mui/joy/Typography/Typography";
+import Box from "@mui/material/Box";
+import Breadcrumbs from "@mui/joy/Breadcrumbs";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+
+import Link from "@mui/joy/Link";
+
+export default function MainPage() {
+  return (
+    <Box sx={{ flex: 1, width: "100%" }}>
+      <Box
+        sx={{
+          position: "sticky",
+          top: { sm: -100, md: -110 },
+          bgcolor: "background.body",
+          zIndex: 9995,
+        }}
+      >
+        <Box sx={{ px: { xs: 2, md: 6 } }}>
+          <Breadcrumbs
+            size="sm"
+            aria-label="breadcrumbs"
+            separator={<ChevronRightRoundedIcon fontSize="small" />}
+            sx={{ pl: 0 }}
+          >
+            <Link
+              underline="none"
+              color="neutral"
+              href="#some-link"
+              aria-label="Home"
+            >
+              <HomeRoundedIcon />
+            </Link>
+            <Link
+              underline="hover"
+              color="neutral"
+              href="#some-link"
+              sx={{ fontSize: 12, fontWeight: 500 }}
+            >
+              Users
+            </Link>
+            <Typography color="primary" sx={{ fontWeight: 500, fontSize: 12 }}>
+              My profile
+            </Typography>
+          </Breadcrumbs>
+          <Typography level="h2" component="h1" sx={{ mt: 1, mb: 2 }}>
+            My profile
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
